054f4b6258b583537ba4d59d9a42bf59621b2af8
owentemp <owentemple@gmail.com>
2016-09-25 16:18:52 -0500

    complete AlbumList component as Functional Component

--- index.ios.js
+++ index.ios.js
@@ -2,13 +2,17 @@
 
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 
 // Create a component
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 
