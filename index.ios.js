1a9755c2c75cccab01863403de23ea3997484152
owentemp <owentemple@gmail.com>
2016-09-25 13:19:37 -0500

    got text rendered on iOS Simulator

--- index.ios.js
+++ index.ios.js
@@ -2,14 +2,15 @@
 
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
-
+import { Text, AppRegistry } from 'react-native';
 
 
 // Create a component
-
+const App = () => (
+    <Text>Some Text</Text>
+);
 
 
 //Render it to the device
-
+AppRegistry.registerComponent('albums', () => App);
 
